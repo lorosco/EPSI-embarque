0f62c3c8d01695fc609e9e9878fed12d14871740
lorosco <leonard.orosco@epsi.fr>
2022-02-24 09:34:24 +0100

    LED  blink using arduino's functions

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -2,8 +2,14 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN,HIGH); // LED on
+  delay(1000); // during 1000ms
+  digitalWrite(LED_BUILTIN,LOW); // LED off
+  delay(1000); // during 1000ms
 }
